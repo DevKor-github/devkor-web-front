40dd7d61dea28f8a0a95780cd606aad5fd0d0faf
KUcodemaster <jh9485@korea.ac.kr>
2023-05-05 22:27:28 +0900

    resolve Merge Conflict

--- devkor_web_front/src/pages/_app.tsx
+++ devkor_web_front/src/pages/_app.tsx
@@ -1,12 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-<<<<<<< HEAD
-=======
-import { Inter } from "next/font/google";
-//import FooterPresentation  from "@/components/main_page/FooterPresentation";
-import '../styles/Footer.css';
-
->>>>>>> origin/feature-
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
