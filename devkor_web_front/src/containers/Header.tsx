e530dc146d7bbbb190472bcdf7590b4b07355430
LJH <shjen3@naver.com>
2023-05-06 12:18:25 +0900

    nav

--- devkor_web_front/src/containers/Header.tsx
+++ devkor_web_front/src/containers/Header.tsx
@@ -1,40 +1,40 @@
 import styled from "styled-components";
-import styles from "./Header.module.css";
+/*import styles from "./Header.module.css";*/
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <div className={styles.contents}>
-                <div>
-                    <Nav>
-                        <ul>
-                            <li>
-                                <a href="/service">서비스</a>
-                            </li>
-                            <li>
-                                <a href="/blog">블로그</a>
-                            </li>
-                            <li>
-                                <a href="/archive">아카이브</a>
-                            </li>
-                        </ul>
-                    </Nav>
-                </div>
-                <div>
-                    <Nav>
-                        <ul>
-                            <li>
-                                <a href="/github">GitHub</a>
-                            </li>
-                            <li>
-                                <a href="/recruiting">리크루팅</a>
-                            </li>
-                        </ul>
-                    </Nav>
-                    {/*<h2>This is Header</h2>*/}
-                </div>
-            </div>
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <div>
+          <Nav>
+            <ul>
+              <li>
+                <a href="/service">서비스</a>
+              </li>
+              <li>
+                <a href="/blog">블로그</a>
+              </li>
+              <li>
+                <a href="/archive">아카이브</a>
+              </li>
+            </ul>
+          </Nav>
+        </div>
+        <div>
+          <Nav>
+            <ul>
+              <li>
+                <a href="/github">GitHub</a>
+              </li>
+              <li>
+                <a href="/recruiting">리크루팅</a>
+              </li>
+            </ul>
+          </Nav>
+          {/*<h2>This is Header</h2>*/}
+        </div>
+      </HeaderContent>
 
-            {/*<div>
+      {/*<div>
                 <nav className={styles.navigation}>
                     <ul>
                         <li>
@@ -54,19 +54,36 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>*/}
-            {/*<h2>This is Header</h2>*/}
-            {/* </div>*/}
-        </header>
-    );
+      {/*<h2>This is Header</h2>*/}
+      {/* </div>*/}
+    </HeaderContainer>
+  );
 };
 export default Header;
 
 const Nav = styled.nav`
-    ul {
-        display: flex;
-        list-style: none;
-        li + li {
-            margin-left: 30px;
-        }
+  ul {
+    display: flex;
+    list-style: none;
+    li + li {
+      margin-left: 30px;
     }
+  }
+`;
+const HeaderContainer = styled.header`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: #ffff;
+`;
+const HeaderContent = styled.div`
+  display: flex;
+  width: 96%;
+  max-width: 1100px;
+  height: 100%;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: space-between;
 `;
