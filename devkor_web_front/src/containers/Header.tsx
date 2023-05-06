373a9a56c9cc7d0ed6ecf16833c8cbaadf14fd0a
KUcodemaster <jh9485@korea.ac.kr>
2023-05-06 11:42:49 +0900

    sass 종속성 문제 해결

--- devkor_web_front/src/containers/Header.tsx
+++ devkor_web_front/src/containers/Header.tsx
@@ -1,38 +1,39 @@
-import styles from './Header.module.scss'
+import styled from "styled-components";
+import styles from "./Header.module.css";
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.contents}>
-            <div>
-                <nav className={styles.navigation}>
-                    <ul>
-                        <li>
-                            <a href="/service">서비스</a>
-                        </li>
-                        <li>
-                            <a href="/blog">블로그</a>
-                        </li>
-                        <li>
-                            <a href="/archive">아카이브</a>
-                        </li>
-                    </ul>
-                </nav>
+                <div>
+                    <Nav>
+                        <ul>
+                            <li>
+                                <a href="/service">서비스</a>
+                            </li>
+                            <li>
+                                <a href="/blog">블로그</a>
+                            </li>
+                            <li>
+                                <a href="/archive">아카이브</a>
+                            </li>
+                        </ul>
+                    </Nav>
+                </div>
+                <div>
+                    <Nav>
+                        <ul>
+                            <li>
+                                <a href="/github">GitHub</a>
+                            </li>
+                            <li>
+                                <a href="/recruiting">리크루팅</a>
+                            </li>
+                        </ul>
+                    </Nav>
+                    {/*<h2>This is Header</h2>*/}
+                </div>
             </div>
-            <div>
-                <nav className={styles.navigation}>
-                    <ul>
-                        <li>
-                            <a href="/github">GitHub</a>
-                        </li>
-                        <li>
-                            <a href="/recruiting">리크루팅</a>
-                        </li>
-                    </ul>
-                </nav>
-                {/*<h2>This is Header</h2>*/}
-            </div>
-            </div>
-            
+
             {/*<div>
                 <nav className={styles.navigation}>
                     <ul>
@@ -53,9 +54,19 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>*/}
-                {/*<h2>This is Header</h2>*/}
-           {/* </div>*/}
+            {/*<h2>This is Header</h2>*/}
+            {/* </div>*/}
         </header>
-    )
-}
-export default Header
+    );
+};
+export default Header;
+
+const Nav = styled.nav`
+    ul {
+        display: flex;
+        list-style: none;
+        li + li {
+            margin-left: 30px;
+        }
+    }
+`;
