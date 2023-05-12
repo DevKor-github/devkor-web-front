139e3738cb42e3086b9669039bf81a2d4135aa03
KUcodemaster <jh9485@korea.ac.kr>
2023-05-12 23:34:26 +0900

    fix: mobile view

---
+++ devkor_web_front/src/components/main_page/mobile_presentation.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import MobileFooter from "./mobileFooter";
+import Paragraph from "@/utils/introParagraph";
+
+export interface BoxProps {
+    Paragraph?: {
+        [key: number]: string[];
+    };
+    ParagraphKey?: number;
+    isMobile?: boolean;
+}
+
+const Box: React.FC<BoxProps> = ({
+    Paragraph,
+    ParagraphKey,
+    isMobile,
+}: BoxProps) => {
+    const [isScrollable, setIsScrollable] = useState(false);
+
+    const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+        const element = e.target as HTMLDivElement;
+        setIsScrollable(element.scrollHeight > element.clientHeight);
+    };
+
+    return (
+        <div>
+            <MobileFooter />
+            <BoxWrapper
+                className={`box ${isMobile ? "mobile" : ""} ${
+                    isScrollable ? "scrollable" : ""
+                }`}
+                onScroll={handleScroll}
+            >
+                <h2>대박</h2>
+            </BoxWrapper>
+        </div>
+    );
+};
+
+const BoxWrapper = styled.div`
+    /* Add your styles here */
+`;
+
+export default Box;
--- devkor_web_front/src/pages/index.tsx
+++ devkor_web_front/src/pages/index.tsx
@@ -2,12 +2,32 @@ import { MainSection } from "@/containers/main_section";
 import FooterContainer from "@/containers/FooterContainer";
 
 import Header from "@/containers/Header";
+import Box from "@/components/main_page/mobile_presentation";
+import { isMobile } from "react-device-detect";
+import { useState, useEffect } from "react";
+
 export default function Home() {
+    const [mobile, setMobile] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isMobile) setMobile(true);
+        else setMobile(false);
+    }, [isMobile]);
+
     return (
         <div>
-            <Header />
-            <MainSection />
-            <FooterContainer />
+            {!mobile && (
+                <div>
+                    <Header />
+                    <MainSection />
+                    <FooterContainer />
+                </div>
+            )}
+            {mobile && (
+                <div>
+                    <Box></Box>
+                </div>
+            )}
         </div>
     );
 }
