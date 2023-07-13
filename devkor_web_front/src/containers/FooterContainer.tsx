61ea40d2fbc609562f6d7a1f2f83549eb3376101
KUcodemaster <jh9485@korea.ac.kr>
2023-07-13 20:17:59 +0900

    fix: revise introduce message

--- devkor_web_front/src/containers/FooterContainer.tsx
+++ devkor_web_front/src/containers/FooterContainer.tsx
@@ -3,8 +3,7 @@ import FooterPresentation from "@/components/main_page/FooterPresentation";
 
 const FooterContainer = () => {
   const URLS = {
-    githubUrl:
-      "https://github.com/DevKor-github/devkor-web-front/tree/main/devkor_web_front",
+    githubUrl: "https://github.com/DevKor-github/",
     instagramUrl: "https://www.instagram.com/devkor.ku/",
     kudogUrl: "https://www.kudog.email/",
     meezzleUrl: "https://www.meezzle.xyz/",
