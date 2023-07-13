87bee0d7c32dcab3e82a997f823272314ac2d1d9
KUcodemaster <jh9485@korea.ac.kr>
2023-07-13 20:13:43 +0900

    Feat: revise some design

--- devkor_web_front/src/containers/FooterContainer.tsx
+++ devkor_web_front/src/containers/FooterContainer.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import FooterPresentation from "@/components/main_page/FooterPresentation";
 
 const FooterContainer = () => {
-    const githubUrl =
-        "https://github.com/DevKor-github/devkor-web-front/tree/main/devkor_web_front";
-    const instagramUrl = "https://www.instagram.com/devkor.ku/";
+  const URLS = {
+    githubUrl:
+      "https://github.com/DevKor-github/devkor-web-front/tree/main/devkor_web_front",
+    instagramUrl: "https://www.instagram.com/devkor.ku/",
+    kudogUrl: "https://www.kudog.email/",
+    meezzleUrl: "https://www.meezzle.xyz/",
+  };
 
-    return (
-        <FooterPresentation githubUrl={githubUrl} instagramUrl={instagramUrl} />
-    );
+  https: return <FooterPresentation urls={URLS} />;
 };
 
 export default FooterContainer;
