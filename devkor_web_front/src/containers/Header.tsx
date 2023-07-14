ee971d63ffc25e815a008369220eb7e08e4b9574
KUcodemaster <jh9485@korea.ac.kr>
2023-07-14 20:09:39 +0900

    Feat: 로그인시 SSO 페이지로 연동

--- devkor_web_front/src/containers/Header.tsx
+++ devkor_web_front/src/containers/Header.tsx
@@ -30,7 +30,14 @@ const Header = () => {
                 <Link href="/recruiting">리크루팅</Link>
               </li>
               <li>
-                <Link href="/login">로그인</Link>
+                <Link
+                  href={`https://devkor-sso-front.vercel.app/`}
+                  passHref
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  로그인
+                </Link>
               </li>
             </ul>
           </Nav>
